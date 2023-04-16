455a5fc14655f543c5c7cb1787ef844575e07b38
Abe Erdos <abe@erdosmiller.com>
2023-04-16 17:17:20 -0500

    Separate sgg helper, full post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,17 +1,51 @@
-import { type NextPage } from "next";
+import { GetStaticProps, type NextPage } from "next";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import Head from "next/head";
 
-const SinglePostPage: NextPage = () => {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) return <div>404</div>;
+
+  console.log(`ProfilePage: id= ${id}`);
+  console.log(`ProfilePage: data= ${JSON.stringify(data)}`);
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("no id");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default SinglePostPage;
